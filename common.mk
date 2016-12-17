0e8d4ab02cc6ac9fa03fa9f56ea6d594dba2a903
nailyk-fr <nailyk_git@nailyk.fr>
2016-12-17 01:49:28 +0100

    common-sodp: build: Remove unneeded configuration
     * ro.hwui.* are inherited from frameworks/native/build
     * gps.conf is not needed anymore
    
    Change-Id: If01ab7609eee2b34c9c03eae2f379a82c2875a55

--- common.mk
+++ common.mk
@@ -62,7 +62,6 @@ PRODUCT_COPY_FILES += \
 # Common etc
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/system/vendor/etc/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    $(COMMON_PATH)/rootdir/system/etc/gps.conf:system/etc/gps.conf \
     $(COMMON_PATH)/rootdir/system/etc/nfcee_access.xml:system/etc/nfcee_access.xml \
     $(COMMON_PATH)/rootdir/system/etc/sec_config:system/etc/sec_config \
     $(COMMON_PATH)/rootdir/system/etc/sensors/sensors_settings:system/etc/sensors/sensors_settings
@@ -269,20 +268,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Hardware composer
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.hwc.mdpcomp.enable=true
-
-# Hardware User Interface parameters
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.texture_cache_size=48 \
-    ro.hwui.layer_cache_size=32 \
-    ro.hwui.r_buffer_cache_size=4 \
-    ro.hwui.path_cache_size=24 \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.drop_shadow_cache_size=5 \
-    ro.hwui.texture_cache_flushrate=0.5 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=1024 \
-    ro.hwui.text_large_cache_width=2048 \
-    ro.hwui.text_large_cache_height=1024
 
 # DRM service
 PRODUCT_PROPERTY_OVERRIDES += \
